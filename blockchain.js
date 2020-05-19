ba8f5921a62fa4619fccacab9f20fdc69e150354
Ashish Kumar Lahoti <lahoti.ashish20@gmail.com>
2020-05-19 15:25:41 +0800

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -2,22 +2,14 @@ const Block = require('./block');
 
 class Blockchain {
 
-    constructor(difficultyLevel) {
-        this.difficultyLevel = difficultyLevel;
+    constructor() {
+        this.difficultyLevel = 1;
         this.chain = [Block.genesis()];
     }
 
     addBlock(record) {
         const newBlock = Block.mineBlock(this.chain[this.chain.length-1], record, this.difficultyLevel);
         this.chain.push(newBlock);
-    }
-
-    setDifficultyLevel(difficultyLevel){
-        this.difficultyLevel = difficultyLevel;
-    }
-
-    print() {
-        console.log(this.chain);
     }
 }
 
