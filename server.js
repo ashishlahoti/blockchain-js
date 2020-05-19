2e5098c525878ea0bfb8181a43ed58987cdf8b19
Ashish Kumar Lahoti <lahoti.ashish20@gmail.com>
2020-05-19 15:26:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 const Blockchain = require('./blockchain')
-const Block = require('./block');
 const { performance } = require('perf_hooks');
 
-const blockchain = new Blockchain(1);
+const blockchain = new Blockchain();
 
 const userList = ["Alice", "Bob", "Charlie", "David", "Eric", "Franklin", "Gavin", "Harry", "Iris", 
                 "Joey", "Kate", "Leo", "Monica", "Nancy", "Oscar", "Phoebe", "Quinn", "Ross", 
                 "Sofia", "Tyler", "Umar", "Victor", "Wilson", "Xena", "Yasmine", "Zara"];
 
+const addNBlocks = (n) => {
+    for(let i = 0; i < n; i++) {
+        blockchain.addBlock({
+            sender: userList[Math.floor(Math.random() * userList.length)],
+            receiver: userList[Math.floor(Math.random() * userList.length)],
+            amount: Math.floor(Math.random() * 1000)
+        });
+    }
+}
+
 const t0 = performance.now();
-for(let i = 0; i < 5; i++) {
-    blockchain.addBlock({
-        sender: userList[Math.floor(Math.random() * userList.length)],
-        receiver: userList[Math.floor(Math.random() * userList.length)],
-        amount: Math.floor(Math.random() * 1000)
-    });
-}
+addNBlocks(2);
 var t1 = performance.now()
-console.log("Added first 5 blocks in  " + (t1 - t0) + " milliseconds.")
+console.log("[Difficulty Level 1] Added first 2 blocks in  " + (t1 - t0) + " milliseconds.")
 
-blockchain.setDifficultyLevel(4);
-for(let i = 0; i < 5; i++) {
-    blockchain.addBlock({
-        sender: userList[Math.floor(Math.random() * userList.length)],
-        receiver: userList[Math.floor(Math.random() * userList.length)],
-        amount: Math.floor(Math.random() * 1000)
-    });
-}
+blockchain.difficultyLevel = 3;
+addNBlocks(2);
 var t2 = performance.now()
-console.log("Added next 5 blocks in  " + (t2 - t1) + " milliseconds.")
+console.log("[Difficulty Level 3] Added next 2 blocks in  " + (t2 - t1) + " milliseconds.")
 
-blockchain.print();
+blockchain.difficultyLevel = 5;
+addNBlocks(2);
+var t3 = performance.now()
+console.log("[Difficulty Level 5] Added next 2 blocks in  " + (t3 - t2) + " milliseconds.")
+
+/* Print Blockchain*/
+console.log(blockchain.chain);
